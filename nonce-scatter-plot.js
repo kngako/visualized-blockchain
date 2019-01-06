e1d9d9bee72fb766ca4a20a59d4f35c6a60f32c6
Yokngako <kngako@ymail.com>
2019-01-07 01:36:47 +0200

    Some foundations to get the scatter plot code ready

---
+++ nonce-scatter-plot.js
@@ -0,0 +1,3 @@
+// TODO: Check out how one can use d3-node to run d3 serverside like how this histogram was rendered serverside here 
+// https://github.com/d3-node/d3-node/blob/master/examples/histogram.js
+// https://beta.observablehq.com/@mbostock/d3-histogram
