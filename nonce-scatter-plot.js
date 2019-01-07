703bfd2ad3d962ff75247edd021b7d4bd4a3f00d
Yokngako <kngako@ymail.com>
2019-01-08 00:39:46 +0200

    The code which produces the scatter plot.
    Narroter: the programmer knows in his heart and soul that he really needs to learn how to use d3 probably.

--- nonce-scatter-plot.js
+++ nonce-scatter-plot.js
@@ -1,3 +1,74 @@
-// TODO: Check out how one can use d3-node to run d3 serverside like how this histogram was rendered serverside here 
-// https://github.com/d3-node/d3-node/blob/master/examples/histogram.js
-// https://beta.observablehq.com/@mbostock/d3-histogram
+// The visualization uses plotly and you will need an account work with their API...
+var config = require('config');
+var plotly = require('plotly')(config.get("plotly.username"), config.get("plotly.api-key"));
+var fs = require('fs');
+
+var Datastore = require('nedb');
+db = {};
+db.blocks = new Datastore({ filename: 'blocks.db', autoload: true });
+
+db.blocks.find({}).sort({height: -1}).exec(function (error, blocks) {
+    console.log("Blocks to visualize: ", blocks.length);
+
+    var maxBlockHeight = blocks.reduce((accumulator, block) => {
+        return accumulator < block.height ? block.height : accumulator; 
+    }, 0); // TODO: Might wanna use Number.MIN_SAFE_VALUE...
+    var minBlockHeight = blocks.reduce((accumulator, block) => {
+        return accumulator > block.height ? block.height : accumulator; 
+    }, maxBlockHeight);
+    blockHeightRange = maxBlockHeight - minBlockHeight;
+
+    var maxBlockNonce = blocks.reduce((accumulator, block) => {
+        return accumulator < block.nonce ? block.nonce : accumulator; 
+    }, 0); // TODO: Might wanna use Number.MIN_SAFE_VALUE...
+    var minBlockNonce = blocks.reduce((accumulator, block) => {
+        return accumulator > block.nonce ? block.nonce : accumulator; 
+    }, maxBlockNonce); // TODO: Might wanna use Number.MAX_SAFE_VALUE...
+    
+    blockNonceRange = maxBlockNonce - minBlockNonce;
+
+    var data = [{
+        x: blocks.map(block => block.height),
+        y: blocks.map(block => block.nonce),
+        mode: 'markers',
+        type: 'scatter',
+        name: 'Nonce to Block height distribution',
+        text: blocks.map(block => block.height),
+        marker: {
+            size: 10
+        },
+    }];
+
+    var figure = { 
+        'data': data, 
+        layout: { 
+            xaxis: {
+                title: 'Height',
+                range: [ minBlockHeight, maxBlockHeight ] 
+            },
+            yaxis: {
+                title: "Nonce",
+                range: [minBlockNonce, maxBlockNonce]
+            },
+            title:'Nonce Visualization'
+        }
+    };
+
+    var imgOpts = {
+        format: 'png',
+        width: 1000,
+        height: 500,
+        // Do some work to keep aspect ratio...
+    };
+
+    plotly.getImage(figure, imgOpts, function (error, imageStream) {
+        if (error) return console.error(error);
+    
+        var fileStream = fs.createWriteStream('nonce.png');
+        imageStream.pipe(fileStream);
+    });
+    // plotly.plot(data, layout, function (err, msg) {
+    //     console.log(msg);
+    // });
+
+});
