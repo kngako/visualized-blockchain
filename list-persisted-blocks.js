0bac926b43b5a19a394ad6649d52912dbba775ae
Kgothatso Ngako <kpangako+github@gmail.com>
2019-01-07 18:07:27 +0200

    Show length in list...

--- list-persisted-blocks.js
+++ list-persisted-blocks.js
@@ -3,7 +3,7 @@ db = {};
 db.blocks = new Datastore({ filename: 'blocks.db', autoload: true });
 
 db.blocks.find({}).sort({height: -1}).exec(function (error, blocks) {
-    console.log("Blocks: ", blocks);
+    console.log("Blocks: ", blocks.length);
 
     blocks.forEach(block => {
         console.log(block.height + "-" + block.hash);
