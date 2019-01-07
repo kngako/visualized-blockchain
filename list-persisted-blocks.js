7a21d7eb890838975cd4b7bb5f4f9e0a5bd6f380
Yokngako <kngako@ymail.com>
2019-01-08 00:44:06 +0200

    Don't need to do that anymore...

--- list-persisted-blocks.js
+++ list-persisted-blocks.js
@@ -5,8 +5,8 @@ db.blocks = new Datastore({ filename: 'blocks.db', autoload: true });
 db.blocks.find({}).sort({height: -1}).exec(function (error, blocks) {
     console.log("Blocks: ", blocks.length);
 
-    blocks.forEach(block => {
-        console.log(block.height + "-" + block.hash);
-    })
+    // blocks.forEach(block => {
+    //     console.log(block.height + "-" + block.hash);
+    // })
 
 });
